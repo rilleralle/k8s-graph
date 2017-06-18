239cc54a8cb2f675c45a02f17174d424dc293e99
Ralf <ralf@stro-bel.de>
2017-06-18 23:24:28 +0200

    Implement graph visualization
    * D3 graph
    * Polling data from k8s api via localhost
    * Use SocketIO to send updates to client

--- server.js
+++ server.js
@@ -1,19 +1,61 @@
 'use strict';
 
-const express = require('express');
-const http = require('http');
+var express = require('express')
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
 const PORT = 3000;
 
-// App
-const app = express();
+function extractInformation(parsedData) {
+    //Circles
+    var minions = parsedData.items.map(function(item) {
+        return item.spec.nodeName;
+    });
+    minions = [...new Set(minions)];
+    var minions = minions.map(function(item) {
+        return {id: item, size: 30, text: item, color: item};
+    });
+    while (minions.length < 4) {
+        minions.push({id: 'dummy'+minions.length, size: 30, color: 'dummy'})
+    }
+    const pods = parsedData.items.map(function(item) {
+        return {
+            id: item.metadata.name,
+            text: item.metadata.name,
+            size: 15,
+            color: item.metadata.labels.app};
+    });
+    const nodes = [{id: 'Master', size: 30, text: 'Master'}].concat(minions).concat(pods);
+
+    //Links
+    const links = parsedData.items.map(function(item) {
+        return {source: item.metadata.name, target: item.spec.nodeName, length: 300};
+    });
+    minions.forEach(function(item) {
+        links.push({source: item.id, target: 'Master', length: 600});
+    });
+
+    return {links: links, nodes: nodes};
+}
+
 app.get('/', function (req, res) {
-    let getRes = res;
+    res.sendFile(__dirname + '/k8s.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    poll();
+});
+
+function poll() {
     http.get('http://127.0.0.1:8001/api/v1/namespaces/default/pods', (res) => {
 
         let error;
         if (res.statusCode !== 200) {
             error = new Error('Request Failed.\n' +
-                `Status Code: ${statusCode}`);
+                'Status Code: ${statusCode}');
         }
         if (error) {
             console.error(error.message);
@@ -28,18 +70,19 @@ app.get('/', function (req, res) {
         res.on('end', () => {
             try {
                 const parsedData = JSON.parse(rawData);
-                console.log(parsedData.items);
-                console.log(parsedData);
-                getRes.send(rawData);
+                const result = extractInformation(parsedData);
+                io.emit('update', result);
             } catch (e) {
+                io.emit('error', e.message);
                 console.error(e.message);
             }
         });
     }).on('error', (e) => {
-        console.error(`Got error: ${e.message}`);
+        io.emit('error', e.message);
+        console.error('Got error: ${e.message}');
     });
+}
+setInterval(poll, 1000);
 
-});
-
-app.listen(PORT);
+server.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
