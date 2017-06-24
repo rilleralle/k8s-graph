6ca9d60c201642f62e3d5eb0cdc917e6a37edb6f
Ralf <ralf@stro-bel.de>
2017-06-25 00:49:41 +0200

    Sticky, dot line, tooltip, externalize config

--- server.js
+++ server.js
@@ -1,41 +1,66 @@
 'use strict';
 
-var express = require('express');
-var http = require('http');
-var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
 
-const PORT = 3000;
+const port = 3000;
+
+const masterSize = getEnvVar('masterSize', 30);
+const minionSize = getEnvVar('minionSize', 30);
+const podSize = getEnvVar('podSize', 15);
+const linkSizePodToMinion = getEnvVar('linkSizePodToMinion', 500);
+const linkSizeMinionToMaster = getEnvVar('linkSizeMinionToMaster', 800);
+const dummyNodes = getEnvVar('dummyNodes', 0);
+const podsApiUrl = getEnvVar('podsApiUrl', 'http://127.0.0.1:8001/api/v1/namespaces/default/pods');
+const pollingIntervalInSeconds = getEnvVar('pollingIntervalInSeconds', 1);
+
+function getEnvVar(envVar, defaultValue) {
+    const value = process.env[envVar];
+    return value === undefined ? defaultValue : value;
+}
 
 function extractInformation(parsedData) {
-    //Circles
-    var minions = parsedData.items.map(function(item) {
+    //Minions, the nodes of the k8s cluster
+    let minions = parsedData.items.map(function(item) {
         return item.spec.nodeName;
     });
     minions = [...new Set(minions)];
     minions = minions.map(function(item) {
-        return {id: item, size: 30, text: item, color: item, type: "node"};
+        return {id: item, size: minionSize, text: item, color: item, type: "Node"};
     });
+
+    //Dummy nodes
     let i = 0;
-    while (minions.length < 1) {
-        minions.push({id: 'dummy'+i++, size: 30, color: 'dummy', type: "node"})
+    while (minions.length < dummyNodes) {
+        minions.push({id: 'dummy'+i++, size: minionSize, color: 'dummy', type: "Node"})
     }
+
+    //Pods
     const pods = parsedData.items.map(function(item) {
         return {
             id: item.metadata.name,
             text: item.metadata.name,
-            size: 15,
-            color: item.metadata.labels.app};
+            size: podSize,
+            color: item.metadata.labels.app,
+            type: "Pod"
+        };
     });
-    const nodes = [{id: 'Master', size: 30, text: 'Master', type: "master"}].concat(minions).concat(pods);
+    //Kubernetes master
+    const k8sMaster = [{id: 'Master', size: masterSize, text: 'Master', type: "Master"}];
+    //
+    const nodes = k8sMaster.concat(minions).concat(pods);
 
     //Links
+    //Pod to minion
     const links = parsedData.items.map(function(item) {
-        return {source: item.metadata.name, target: item.spec.nodeName, length: 500};
+        return {source: item.metadata.name, target: item.spec.nodeName, length: linkSizePodToMinion, dotted: true};
     });
+    //Minion to master
     minions.forEach(function(item) {
-        links.push({source: item.id, target: 'Master', length: 800});
+        links.push({source: item.id, target: 'Master', length: linkSizeMinionToMaster, dotted: false});
     });
 
     return {links: links, nodes: nodes};
@@ -45,45 +70,35 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/k8s.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+io.on('connection', function(){
     poll();
 });
 
 function poll() {
-    http.get('http://127.0.0.1:8001/api/v1/namespaces/default/pods', (res) => {
-
-        let error;
-        if (res.statusCode !== 200) {
-            error = new Error('Request Failed.\n' +
-                'Status Code: ${statusCode}');
-        }
-        if (error) {
-            console.error(error.message);
-            // consume response data to free up memory
-            res.resume();
-            return;
-        }
-
+    http.get(podsApiUrl, (res) => {
         res.setEncoding('utf8');
         let rawData = '';
         res.on('data', (chunk) => { rawData += chunk; });
         res.on('end', () => {
             try {
-                const parsedData = JSON.parse(rawData);
-                const result = extractInformation(parsedData);
+                const result = extractInformation(JSON.parse(rawData));
                 io.emit('update', result);
             } catch (e) {
-                io.emit('error', e.message);
-                console.error(e.message);
+                handleError('Unable to parse and extract information from k8s response.\n'
+                    + `Error message: ${e.message}`);
             }
         });
     }).on('error', (e) => {
-        io.emit('error', e.message);
-        console.error('Got error: ${e.message}');
+        handleError(`Request to k8s failed.\nError message: ${e.message}`);
     });
 }
+
+function handleError(errorMessage) {
+    console.error(errorMessage);
+    io.emit('error', errorMessage);
+}
+
 setInterval(poll, 1000);
 
-server.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+server.listen(port);
+console.log('Running on http://localhost:' + port);
