882df71de868b607523f9e6617a0ff5a0c6f0098
Ralf <ralf@stro-bel.de>
2018-03-02 20:23:47 +0100

    Fix

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t rilleralle/k8s-graph:develop ../.
+docker build -t rilleralle/k8s-graph:develop .
 #docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
 #docker push rilleralle/k8s-graph:develop
